b9d3d381e1df5d2fe00ceee48752e5777e2d10cd
Halil Özaktaş <h.ozaktas@gmail.com>
2019-01-15 23:01:55 +0300

    yeap

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/dji/tello"
+)
+
+func main() {
+	drone := tello.NewDriver("8888")
+
+	work := func() {
+		mplayer := exec.Command("mplayer", "-fps", "25", "-")
+		mplayerIn, _ := mplayer.StdinPipe()
+		if err := mplayer.Start(); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		drone.On(tello.ConnectedEvent, func(data interface{}) {
+			fmt.Println("Connected")
+			drone.StartVideo()
+			drone.SetVideoEncoderRate(4)
+			gobot.Every(100*time.Millisecond, func() {
+				drone.StartVideo()
+			})
+		})
+
+		drone.On(tello.VideoFrameEvent, func(data interface{}) {
+			pkt := data.([]byte)
+			if _, err := mplayerIn.Write(pkt); err != nil {
+				fmt.Println(err)
+			}
+		})
+
+		drone.TakeOff()
+
+		gobot.After(3*time.Second, func() {
+			drone.Forward(20)
+		})
+
+		gobot.After(6*time.Second, func() {
+			drone.Backward(10)
+		})
+
+		gobot.After(6*time.Second, func() {
+			drone.Left(5)
+		})
+
+		gobot.After(15*time.Second, func() {
+			drone.Land()
+		})
+	}
+
+	robot := gobot.NewRobot("tello",
+		[]gobot.Connection{},
+		[]gobot.Device{drone},
+		work,
+	)
+
+	robot.Start()
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Dji Tello Drone - GoLang Examples
+
+## Examples
+
+- Video Stream
+- Drone Control
+- TakeOff & Land
+
+## Requirements
+
+- mplayer
+- gobot.io/x/gobot 
+- gobot.io/x/gobot/platforms/dji/tello
+
+```sh
+$ brew install mplayer
+$ go get gobot.io/x/gobot 
+$ go get gobot.io/x/gobot/platforms/dji/tello
+```
+
+### .... to be continued
